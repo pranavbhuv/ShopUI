524ec8a407df4e613a6ab804b7287d92588e9ae7
DaPigGuy <mcpepig123@gmail.com>
2018-01-15 20:35:56 -0800

    Reformat everything.

--- src/QuiverlyRivalry/QuiverlyRivalry.php
+++ src/QuiverlyRivalry/QuiverlyRivalry.php
@@ -1,17 +1,17 @@
 <?php
+
 namespace QuiverlyRivalry;
 
-# Main uses:
+use pocketmine\command\{
+    Command, CommandSender
+};
+use pocketmine\item\Item;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
-#Utils uses:
 use pocketmine\utils\TextFormat;
-#Item uses:
-use pocketmine\item\Item;
-#Command uses:
-use pocketmine\command\{Command, CommandSender};
 
-class QuiverlyRivalry extends PluginBase{
+class QuiverlyRivalry extends PluginBase
+{
     private $economyapi;
     private $formapi;
 
@@ -26,11 +26,12 @@ class QuiverlyRivalry extends PluginBase{
 
     public $nomoney = TextFormat::RED . "You do not have enough money!";
 
-    public function onEnable(){
-        foreach(['FormAPI', 'EconomyAPI'] as $depend){
+    public function onEnable()
+    {
+        foreach (['FormAPI', 'EconomyAPI'] as $depend) {
             $plugin = $this->getServer()->getPluginManager()->getPlugin($depend);
             $name = strtolower($depend);
-            if(is_null($plugin)){
+            if (is_null($plugin)) {
                 $this->getLogger()->error($depend . " is required to use this plugin.");
                 $this->setEnabled(false);
                 return false;
@@ -38,16 +39,17 @@ class QuiverlyRivalry extends PluginBase{
             $this->$name = $plugin;
         }
         $this->saveDefaultConfig();
-        foreach (["weapons", "tools", "armor", "blocks", "specials", "masks"] as $category){
+        foreach (["weapons", "tools", "armor", "blocks", "specials", "masks"] as $category) {
             $this->$category = $this->getConfig()->getNested("items." . $category);
         }
         $this->getLogger()->info("ShopUI by Quiverly! Remember I am a developer for hire!");
     }
 
-    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool{
-        switch ($cmd->getName()){
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool
+    {
+        switch ($cmd->getName()) {
             case "shopui":
-                if($sender instanceof Player){
+                if ($sender instanceof Player) {
                     $this->mainForm($sender);
                     return true;
                 }
@@ -57,12 +59,13 @@ class QuiverlyRivalry extends PluginBase{
         return true;
     }
 
-    public function mainForm(Player $player){
-        $form = $this->formapi->createSimpleForm(function (Player $player, array $data){
-            if(isset($data[0])){
+    public function mainForm(Player $player)
+    {
+        $form = $this->formapi->createSimpleForm(function (Player $player, array $data) {
+            if (isset($data[0])) {
                 $result = $data[0];
                 $categories = [0 => "weapons", 1 => "tools", 2 => "armor", 3 => "blocks", 4 => "specials", 5 => "masks"];
-                switch($result){
+                switch ($result) {
                     case 6:
                         return;
                     default:
@@ -76,30 +79,31 @@ class QuiverlyRivalry extends PluginBase{
         $form->setTitle(TextFormat::WHITE . "--= " . TextFormat::BOLD . TextFormat::GREEN . "CastleRaid" . TextFormat::RESET . TextFormat::WHITE . " =--");
         $money = $this->economyapi->myMoney($player->getName());
         $form->setContent("Your Money: $" . $money);
-        $form->addButton(TextFormat::WHITE."Weapons");
-        $form->addButton(TextFormat::WHITE."Tools");
-        $form->addButton(TextFormat::WHITE."Armour");
-        $form->addButton(TextFormat::WHITE."Blocks");
-        $form->addButton(TextFormat::WHITE."Special Items");
-        $form->addButton(TextFormat::WHITE."Masks");
-        $form->addButton(TextFormat::GREEN."Exit");
+        $form->addButton(TextFormat::WHITE . "Weapons");
+        $form->addButton(TextFormat::WHITE . "Tools");
+        $form->addButton(TextFormat::WHITE . "Armour");
+        $form->addButton(TextFormat::WHITE . "Blocks");
+        $form->addButton(TextFormat::WHITE . "Special Items");
+        $form->addButton(TextFormat::WHITE . "Masks");
+        $form->addButton(TextFormat::GREEN . "Exit");
         $form->sendToPlayer($player);
     }
 
-    public function categoryForm(Player $player, $category){
-        $form = $this->formapi->createSimpleForm(function (Player $player, array $data){
-            if(isset($data[0])){
+    public function categoryForm(Player $player, $category)
+    {
+        $form = $this->formapi->createSimpleForm(function (Player $player, array $data) {
+            if (isset($data[0])) {
                 $category = $this->category[$player->getLowerCaseName()];
-                if($data[0] < count($this->$category)){
+                if ($data[0] < count($this->$category)) {
                     $item = $this->$category[$data[0]];
                     $values = explode(":", $item);
                     $money = $this->economyapi->myMoney($player->getName());
-                    if($money >= $values[4]){
+                    if ($money >= $values[4]) {
                         $player->getInventory()->addItem(Item::get($values[0], $values[1], $values[2])->setCustomName($values[3]));
                         $this->economyapi->reduceMoney($player, $values[4]);
                         $player->sendMessage("You have bought " . $values[2] . " " . $values[3] . " for $" . $values[4]);
                     }
-                }else{
+                } else {
                     $this->mainForm($player);
                 }
             }
@@ -107,7 +111,7 @@ class QuiverlyRivalry extends PluginBase{
         $form->setTitle(ucfirst($category));
         $money = $this->economyapi->myMoney($player->getName());
         $form->setContent("Your Money: " . $money);
-        foreach ($this->$category as $item){
+        foreach ($this->$category as $item) {
             $values = explode(":", $item);
             $form->addButton($values[3] . " : " . $values[4], isset($values[5]) ? $values[5] : -1, isset($values[6]) ? $values[6] : "");
         }
